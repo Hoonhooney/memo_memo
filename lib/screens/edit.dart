87459c0a5146940057c475bdd5676d87f1e0b94b
Hoonhooney <xogns0520@ajou.ac.kr>
2020-06-12 18:09:54 +0900

    TextField 추가

--- lib/screens/edit.dart
+++ lib/screens/edit.dart
@@ -3,6 +3,41 @@ import 'package:flutter/material.dart';
 class EditPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.blue,);
+    return Scaffold(
+      appBar: AppBar(
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.delete),
+            onPressed: (){},
+          ),
+          IconButton(
+            icon: const Icon(Icons.save),
+            onPressed: (){},
+          )
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(30),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
+              keyboardType: TextInputType.multiline,
+              maxLines: null,
+              decoration: InputDecoration(
+                labelText: "Title",
+              ),
+            ),
+            TextField(
+              keyboardType: TextInputType.multiline,
+              maxLines: null,
+              decoration: InputDecoration(
+                labelText: "Text",
+              ),
+            ),
+          ],
+        ),
+      )
+    );
   }
 }
