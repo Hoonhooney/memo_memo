514d87da597666026978114be5127f34f8d28887
Hoonhooney <xogns0520@ajou.ac.kr>
2020-06-13 17:30:23 +0900

    홈화면에 data listView에 나열하기

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:memomemo/database/memo.dart';
 import 'edit.dart';
+import 'package:memomemo/database/db.dart';
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -12,37 +14,27 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        physics: BouncingScrollPhysics(),
+      body: Column(
         children: <Widget>[
-          Row(
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.only(left: 20, top: 20, bottom: 20),
-                child: Text('Memo Memo', style: TextStyle(fontSize: 36,
-                    color: Colors.blue)),
-              )
-            ],
+          Padding(
+            padding: EdgeInsets.only(left: 20, top: 20, bottom: 20),
+            child: Text('Memo Memo', style: TextStyle(fontSize: 36,
+                color: Colors.deepOrange)),
           ),
-          ...loadMemos()
-        ] ,
+          Expanded(child: memoBuilder()),
+        ],
       ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: (){
           Navigator.push(context,
             CupertinoPageRoute(builder: (context) => EditPage())
-          );
+          ).then((value) {
+            setState(() {});
+          });
         },
         tooltip: 'Tab if you want to add new note',
         label: Text('Add New Note'),
@@ -51,9 +43,32 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  List<Widget> loadMemos() {
-    List<Widget> memoList = [];
-    memoList.add(Container(color: Colors.red, height: 100,));
-    return memoList;
+  Future<List<Memo>>loadMemo() async{
+    DBHelper helper = DBHelper();
+    return await helper.memos();
+  }
+
+  Widget memoBuilder() {
+    return FutureBuilder(
+      builder: (context, snap) {
+        if (snap.hasData == null) {
+          return Container(child: Text('Try writing a new memo!'),);
+        }
+        return ListView.builder(
+          itemCount: snap.data.length,
+          itemBuilder: (context, index) {
+            Memo memo = snap.data[index];
+            return Column(
+              children: <Widget>[
+                Text(memo.title),
+                Text(memo.editedAt),
+                Text(memo.text),
+              ],
+            );
+          },
+        );
+      },
+      future: loadMemo(),
+    );
   }
 }
